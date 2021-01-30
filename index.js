e4c2044d574f7bcecd61da93393e0575160d657e
Artemy <48246993+quertc@users.noreply.github.com>
2021-01-30 22:33:15 +0300

    feat: add the main code

---
+++ index.js
@@ -0,0 +1,25 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+const { EOL } = require('os');
+
+let inputString = '';
+let currentLine = 0;
+
+function readline() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  const foo = readline();
+  console.log(foo);
+}
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', () => {
+  inputString = inputString.split(EOL).map(str => str.trim());
+  main();
+});
